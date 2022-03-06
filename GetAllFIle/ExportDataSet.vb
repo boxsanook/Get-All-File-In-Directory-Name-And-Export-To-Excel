e08face02e45d8c670cfa1cb76a0cec872842258
boxsanook <37097193+boxsanook@users.noreply.github.com>
2022-03-06 21:52:56 +0700

    Add files via upload

---
+++ GetAllFIle/ExportDataSet.vb
@@ -0,0 +1,111 @@
+﻿Imports DocumentFormat.OpenXml.Packaging
+
+Module ExportData
+    'https://stackoverflow.com/questions/41454836/vb-net-datatable-to-excel
+    Public Sub ExportDataSet(ByVal DataTable_In As DataTable, ByVal Destination As String, Optional FileName As String = "ExcelFileName.xlsx", Optional ds As DataSet = Nothing)
+
+        Try
+            Using workbook = SpreadsheetDocument.Create(Destination & "\" & FileName, DocumentFormat.OpenXml.SpreadsheetDocumentType.Workbook)
+                Dim workbookPart = workbook.AddWorkbookPart()
+                workbook.WorkbookPart.Workbook = New DocumentFormat.OpenXml.Spreadsheet.Workbook()
+                workbook.WorkbookPart.Workbook.Sheets = New DocumentFormat.OpenXml.Spreadsheet.Sheets()
+
+                If Not DataTable_In Is Nothing Then
+
+                    Dim sheetPart = workbook.WorkbookPart.AddNewPart(Of WorksheetPart)()
+                    Dim sheetData = New DocumentFormat.OpenXml.Spreadsheet.SheetData()
+                    sheetPart.Worksheet = New DocumentFormat.OpenXml.Spreadsheet.Worksheet(sheetData)
+                    Dim sheets As DocumentFormat.OpenXml.Spreadsheet.Sheets = workbook.WorkbookPart.Workbook.GetFirstChild(Of DocumentFormat.OpenXml.Spreadsheet.Sheets)()
+                    Dim relationshipId As String = workbook.WorkbookPart.GetIdOfPart(sheetPart)
+                    Dim sheetId As UInteger = 1
+
+                    If sheets.Elements(Of DocumentFormat.OpenXml.Spreadsheet.Sheet)().Count() > 0 Then
+                        sheetId = sheets.Elements(Of DocumentFormat.OpenXml.Spreadsheet.Sheet)().[Select](Function(s) s.SheetId.Value).Max() + 1
+                    End If
+
+                    Dim sheet As DocumentFormat.OpenXml.Spreadsheet.Sheet = New DocumentFormat.OpenXml.Spreadsheet.Sheet() With {
+                    .Id = relationshipId,
+                    .SheetId = sheetId,
+                    .Name = DataTable_In.TableName
+                 }
+                    sheets.Append(sheet)
+                    Dim headerRow As DocumentFormat.OpenXml.Spreadsheet.Row = New DocumentFormat.OpenXml.Spreadsheet.Row()
+                    Dim columns As List(Of String) = New List(Of String)()
+
+                    For Each column As System.Data.DataColumn In DataTable_In.Columns
+                        columns.Add(column.ColumnName)
+                        Dim cell As DocumentFormat.OpenXml.Spreadsheet.Cell = New DocumentFormat.OpenXml.Spreadsheet.Cell()
+                        cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String
+                        cell.CellValue = New DocumentFormat.OpenXml.Spreadsheet.CellValue(column.ColumnName)
+                        headerRow.AppendChild(cell)
+                    Next
+
+                    sheetData.AppendChild(headerRow)
+
+                    For Each dsrow As System.Data.DataRow In DataTable_In.Rows
+                        Dim newRow As DocumentFormat.OpenXml.Spreadsheet.Row = New DocumentFormat.OpenXml.Spreadsheet.Row()
+
+                        For Each col As String In columns
+                            Dim cell As DocumentFormat.OpenXml.Spreadsheet.Cell = New DocumentFormat.OpenXml.Spreadsheet.Cell()
+                            cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String
+                            cell.CellValue = New DocumentFormat.OpenXml.Spreadsheet.CellValue(dsrow(col).ToString())
+                            newRow.AppendChild(cell)
+                        Next
+
+                        sheetData.AppendChild(newRow)
+                    Next
+                Else
+                    For Each table As System.Data.DataTable In ds.Tables
+                        Dim sheetPart = workbook.WorkbookPart.AddNewPart(Of WorksheetPart)()
+                        Dim sheetData = New DocumentFormat.OpenXml.Spreadsheet.SheetData()
+                        sheetPart.Worksheet = New DocumentFormat.OpenXml.Spreadsheet.Worksheet(sheetData)
+                        Dim sheets As DocumentFormat.OpenXml.Spreadsheet.Sheets = workbook.WorkbookPart.Workbook.GetFirstChild(Of DocumentFormat.OpenXml.Spreadsheet.Sheets)()
+                        Dim relationshipId As String = workbook.WorkbookPart.GetIdOfPart(sheetPart)
+                        Dim sheetId As UInteger = 1
+
+                        If sheets.Elements(Of DocumentFormat.OpenXml.Spreadsheet.Sheet)().Count() > 0 Then
+                            sheetId = sheets.Elements(Of DocumentFormat.OpenXml.Spreadsheet.Sheet)().[Select](Function(s) s.SheetId.Value).Max() + 1
+                        End If
+
+                        Dim sheet As DocumentFormat.OpenXml.Spreadsheet.Sheet = New DocumentFormat.OpenXml.Spreadsheet.Sheet() With {
+                        .Id = relationshipId,
+                        .SheetId = sheetId,
+                        .Name = table.TableName
+                     }
+                        sheets.Append(sheet)
+                        Dim headerRow As DocumentFormat.OpenXml.Spreadsheet.Row = New DocumentFormat.OpenXml.Spreadsheet.Row()
+                        Dim columns As List(Of String) = New List(Of String)()
+
+                        For Each column As System.Data.DataColumn In table.Columns
+                            columns.Add(column.ColumnName)
+                            Dim cell As DocumentFormat.OpenXml.Spreadsheet.Cell = New DocumentFormat.OpenXml.Spreadsheet.Cell()
+                            cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String
+                            cell.CellValue = New DocumentFormat.OpenXml.Spreadsheet.CellValue(column.ColumnName)
+                            headerRow.AppendChild(cell)
+                        Next
+
+                        sheetData.AppendChild(headerRow)
+
+                        For Each dsrow As System.Data.DataRow In table.Rows
+                            Dim newRow As DocumentFormat.OpenXml.Spreadsheet.Row = New DocumentFormat.OpenXml.Spreadsheet.Row()
+
+                            For Each col As String In columns
+                                Dim cell As DocumentFormat.OpenXml.Spreadsheet.Cell = New DocumentFormat.OpenXml.Spreadsheet.Cell()
+                                cell.DataType = DocumentFormat.OpenXml.Spreadsheet.CellValues.String
+                                cell.CellValue = New DocumentFormat.OpenXml.Spreadsheet.CellValue(dsrow(col).ToString())
+                                newRow.AppendChild(cell)
+                            Next
+
+                            sheetData.AppendChild(newRow)
+                        Next
+                    Next
+                End If
+
+
+            End Using
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+End Module
---
+++ GetAllFIle/FRM_GetAllFile.vb
@@ -0,0 +1,75 @@
+﻿Imports System.IO
+
+Public Class FRM_GetAllFile
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+            Getall(TextBox1.Text)
+        End If
+
+
+        'Dim baseDirectory As String = "F:\Document Control\"
+        'Dim allFiles As String() = Directory.GetFiles(baseDirectory, "*.*", SearchOption.AllDirectories)
+        'For Each file In allFiles
+        '    ListBox1.Items.Add(file)
+        'Next
+    End Sub
+    Public Sub Getall(FolderName As String)
+        ''''https://docs.microsoft.com/en-us/dotnet/api/system.io.fileinfo?view=net-6.0
+        Dim strFileSize As String = ""
+        Dim di As New IO.DirectoryInfo(FolderName)
+        Dim aryFi As IO.FileInfo() = di.GetFiles("*.*", SearchOption.AllDirectories)
+        Dim fi As IO.FileInfo
+        TestOrder()
+        ClearDataGrid()
+        Dim id As Integer = 0
+        For Each fi In aryFi
+            id = id + 1
+            strFileSize = (Math.Round(fi.Length / 1024)).ToString()
+            FileDirectory.Rows.Add(id, fi.DirectoryName, fi.FullName, fi.Name, strFileSize, fi.Extension, fi.LastAccessTime, (fi.Attributes.ReadOnly = True).ToString)
+
+            'Console.WriteLine("File Name: {0}", fi.Name)
+            'Console.WriteLine("File Full Name: {0}", fi.FullName)
+            'Console.WriteLine("File Size (KB): {0}", strFileSize)
+            'Console.WriteLine("File Extension: {0}", fi.Extension)
+            'Console.WriteLine("Last Accessed: {0}", fi.LastAccessTime)
+            'Console.WriteLine("Read Only: {0}", (fi.Attributes.ReadOnly = True).ToString)
+        Next
+        DataGridView1.DataSource = FileDirectory
+    End Sub
+    Public Sub ClearDataGrid()
+        DataGridView1.CancelEdit()
+        DataGridView1.Columns.Clear()
+        DataGridView1.DataSource = Nothing
+        '///  Button2 Edit File
+
+    End Sub
+    Dim FileDirectory As New DataTable
+    Public Sub TestOrder()
+        FileDirectory = New DataTable
+        FileDirectory.Columns.Add(New DataColumn("id", GetType(Integer)))
+        FileDirectory.Columns.Add(New DataColumn("DirectoryName", GetType(String)))
+        FileDirectory.Columns.Add(New DataColumn("Full Directory FileName", GetType(String)))
+        FileDirectory.Columns.Add(New DataColumn("File Name", GetType(String)))
+        FileDirectory.Columns.Add(New DataColumn("File Size (KB)", GetType(Decimal)))
+        FileDirectory.Columns.Add(New DataColumn("File Extension", GetType(String)))
+        FileDirectory.Columns.Add(New DataColumn("Last Accessed", GetType(String)))
+        FileDirectory.Columns.Add(New DataColumn("Read Only", GetType(String)))
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If DataGridView1.RowCount() > 0 Then
+            If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+                ExportDataSet(FileDirectory, FolderBrowserDialog1.SelectedPath)
+            End If
+        Else
+            MessageBox.Show("No Data Export", "error !!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+End Class
